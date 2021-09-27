c9240d4c0daa8200b6ffc0896cf7c689679607d4
Sławomir Woźniak <slawomirw7@hotmail.com>
2021-09-27 20:09:18 +0200

    Add project files.

---
+++ FizzBuzz.cs
@@ -0,0 +1,15 @@
+﻿namespace FizzBuzzGame
+{
+    public class FizzBuzz
+    {
+        public string GetResult(int number)
+        {
+            string result = "";
+            if (number % 3 == 0) result = "Fizz";
+            if (number % 5 == 0) result = result + "Buzz";
+            if (string.IsNullOrWhiteSpace(result)) result = number.ToString();
+
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzzGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            Console.WriteLine("*** Witaj w grze FizzBuzz ***");
+
+            while (true)
+            {
+                Console.WriteLine(fizzBuzz.GetResult(GetNumber()));
+
+                Console.WriteLine("Czy chcesz kontynuować grę? Jeśli tak to naciśnij \'T\'");
+                if (Console.ReadLine().ToUpper() != "T") break;
+            }
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                Console.WriteLine("\nPodaj liczbę całkowitą:");
+                if (int.TryParse(Console.ReadLine(), out int number))
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.WriteLine("Podano nieprawidłowe dane.");
+                }
+            }
+        }
+    }
+}
